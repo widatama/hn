8d4f697d8b71d35c3e853b759e669285166ebd71
Danu Widatama <widatama@gmail.com>
2024-02-14 22:58:45 +0700

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  distDir: 'dist',
   eslint: {
     dirs: ['src'],
   },
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,9 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
-import logger from 'redux-logger';
 
 import hnApi from '@/store/api/hn';
 
 const middleware = [...getDefaultMiddleware(), hnApi.middleware];
-
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(logger);
-}
 
 function makeStore() {
   return configureStore({
