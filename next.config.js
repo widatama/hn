8e7060867c65b2bfd97e38a87e6b6f51e78c3602
Danu Widatama <widatama@gmail.com>
2026-01-12 13:18:57 +0700

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  eslint: {
-    dirs: ['src'],
-    ignoreDuringBuilds: true,
-  },
   output: 'export',
 };
